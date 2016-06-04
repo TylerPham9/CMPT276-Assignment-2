750ba489123a68ec85974f9cbd25c399ce08fdfc
Tyler Pham <tylerpham.9@gmail.com>
2016-06-04 19:37:52 +0000

    public assets removed

--- public/assets/draw-8138026a773512d5927f94fe77572d880a9f0f167ea71f1df4c71b2db27efb7a.js
+++
@@ -1,1 +0,0 @@
-function sortGroups(e,n){var t=document.getElementById("group"+n),o=document.getElementById("boxId_"+e);t.appendChild(o)}$(document).ready(function(){$(".groupContainers").click(function(){$(this).children(".studentBoxes").toggle("slow")})});
