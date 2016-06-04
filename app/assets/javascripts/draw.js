6838b469bd4cb7dbbe03d9279930a9b1caab4152
Tyler Pham <tylerpham.9@gmail.com>
2016-06-04 19:30:15 +0000

    precompile

--- app/assets/javascripts/draw.js
+++ app/assets/javascripts/draw.js
@@ -2,7 +2,8 @@ function sortGroups(studentId, groupNum) {
     var groupDiv = document.getElementById("group"+groupNum);
     var studentDiv = document.getElementById("boxId_"+studentId);
     groupDiv.appendChild(studentDiv);
-    
+    // console.log("foo");
+
 }
 
 
